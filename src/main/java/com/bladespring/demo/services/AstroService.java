42d101f9257b3d56e80e4695e4d8a0bc20835420
Blessed Sibanda <blessedsibanda.spring@gmail.com>
2023-06-10 11:34:17 +0200

    Use the Reactive WebClient

--- src/main/java/com/bladespring/demo/services/AstroService.java
+++ src/main/java/com/bladespring/demo/services/AstroService.java
@@ -1,23 +1,38 @@
 package com.bladespring.demo.services;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.bladespring.demo.json.AstroResult;
 
 @Service
 public class AstroService {
     private final RestTemplate template;
+    private final WebClient client;
 
     @Autowired
-    public AstroService(RestTemplateBuilder rtBuilder) {
+    public AstroService(RestTemplateBuilder rtBuilder, WebClient.Builder wcBuilder) {
         template = rtBuilder.build();
+        client = wcBuilder.baseUrl("http://api.open-notify.org").build();
     }
 
-    public AstroResult getAstronauts() {
+    public AstroResult getAstronautsRT() {
         String url = "http://api.open-notify.org/astros.json";
         return template.getForObject(url, AstroResult.class);
+    }
+
+    public AstroResult getAstronautsWC() {
+        return client.get()
+                .uri("/astros.json")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(AstroResult.class)
+                .block(Duration.ofSeconds(2));
     }
 }
--- src/test/java/com/bladespring/demo/services/AstroServiceTest.java
+++ src/test/java/com/bladespring/demo/services/AstroServiceTest.java
@@ -19,8 +19,20 @@ public class AstroServiceTest {
     private AstroService service;
 
     @Test
-    void getAstronauts() {
-        AstroResult result = service.getAstronauts();
+    void getAstronautsRT() {
+        AstroResult result = service.getAstronautsRT();
+        int number = result.number();
+        System.out.println("There are " + number + " people in space");
+        List<Asssignment> people = result.people();
+        people.forEach(System.out::println);
+        assertAll(
+                () -> assertTrue(number >= 0),
+                () -> assertEquals(number, people.size()));
+    }
+
+    @Test
+    void getAstronautsWC() {
+        AstroResult result = service.getAstronautsWC();
         int number = result.number();
         System.out.println("There are " + number + " people in space");
         List<Asssignment> people = result.people();
